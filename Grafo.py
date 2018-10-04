4baf901d2c5c58afbac39810e837801fe72809cb
Aryell Dias <aryelld@gmail.com>
2018-10-04 00:03:14 -0300

    rota automática simples

--- Grafo.py
+++ Grafo.py
@@ -1,32 +1,55 @@
 import numpy as np
 # classe Vertice
 class V:
-    # vertice "" esta vazio
+    # pode ser adicionado feromonio 
+    # quando se implementar o ACO
     def __init__(self, tipo):
-        self.visitado = tipo == "" or tipo == "1"
+        # onde R representa a localização do robo interventor
+        self.visitado = tipo == "1"
         self.tipo = tipo
+    def visitar(self):
+        self.visitado = True
     def toString(self):
         return self.tipo
 # classe Aresta
 class A:
-    def __init__(self, vertice1, vertice2):
+    def __init__(self, vertice1, vertice2, acao):
         self.vertice1 = vertice1
         self.vertice2 = vertice2
+        self.acao = acao
     def toString(self):
         return self.vertice1.toString() + "->" + self.vertice2.toString()
 # classe Grafo
 class G:
     def __init__(self,mapa):
-        a = self.mpV(mapa)
-        self.mapaVertice = a
+        self.mapaVertice = self.mpV(mapa)
+        jMax = np.array(self.mapaVertice[0]).size 
+        iMax = np.array(self.mapaVertice).size/jMax
         self.grafo = {}
-        i = 0
-        for linha in a:
-            j = 0
+        i = 0 
+        for linha in self.mapaVertice:
+            j = 0 
             for pos in linha:
-                self.grafo[pos] = [A(pos, adjacente) for adjacente in filter(lambda x : x.tipo != "",self.vizinhos(i,j))]
+                if(pos.tipo == '0'):
+                    self.grafo[pos] = []
+                    if(i>0):
+                        aux = self.mapaVertice[i-1][j]
+                        if(aux.tipo == '0' or aux.tipo == '-1'):
+                            self.grafo[pos] += [A(pos, aux, 'acima')] 
+                    if(j>0):
+                        aux = self.mapaVertice[i][j-1]
+                        if(aux.tipo == '0' or aux.tipo == '-1'):
+                            self.grafo[pos] += [A(pos, aux, 'atras')]
+                    if(i<iMax-1):
+                        aux = self.mapaVertice[i+1][j]
+                        if(aux.tipo == '0' or aux.tipo == '-1'):
+                            self.grafo[pos] += [A(pos, aux, 'abaixo')]
+                    if(j<jMax-1):
+                        aux = self.mapaVertice[i][j+1]
+                        if(aux.tipo == '0' or aux.tipo == '-1'):
+                            self.grafo[pos] += [A(pos, aux, 'afrente')]
                 j+=1
-            i+=1
+            i+=1     
     def mpV(self,mapa):
         retorno = []
         for line in mapa:
@@ -35,33 +58,10 @@ class G:
                 linha.append(V(termo))
             retorno.append(linha)
         return retorno
-    def vizinhos(self,i,j):
-        jMax = np.array(self.mapaVertice[0]).size 
-        iMax = np.array(self.mapaVertice).size/jMax
-        retorno = []
-        if(i>0):
-            retorno.append(self.mapaVertice[i-1][j])
-        else:
-            retorno.append(V(""))
-        if(j>0):
-            retorno.append(self.mapaVertice[i][j-1])
-        else:
-            retorno.append(V(""))
-        if(j<jMax-1):
-            retorno.append(self.mapaVertice[i][j+1])
-        else:
-            retorno.append(V(""))
-        if(i<iMax-1):
-            retorno.append(self.mapaVertice[i+1][j])
-        else:
-            retorno.append(V(""))
-        # vizinho antes em i,j vizinho depois i,j
-        return retorno
     def toString(self):
-        for linha in self.mapaVertice:
-            for vertice in linha:
-                lista = []
-                for aresta in self.grafo[vertice]:
-                    lista.append(aresta.toString())
-                print("{"+vertice.toString()+":"+lista.__str__()+"}")
-    # def custo(self, i,j):
+        for vertice in self.grafo.keys():
+            lista = []
+            for aresta in self.grafo[vertice]:
+                lista.append(aresta.toString())
+            print("{"+vertice.toString()+":"+lista.__str__()+"}")
+        
--- teste.py
+++ teste.py
@@ -1,8 +1,17 @@
 import Grafo as g
 # onde R representa a localização atual do robo
 # interventor
-mapa = [['R','0','0','0','0','0'],
-        ['0','1','0','1','1','0'],
-        ['0','0','0','0','1','-1']]
-grafo = g.G(mapa)
-print(grafo.toString())
+# mapa = [['R','0','0','0','0','0'],
+#         ['0','1','0','1','1','1'],
+#         ['0','0','0','0','1','-1']]
+# o mapa terá partes inacessíveis marcadas,
+# isoladas por 1s sempre, então é adequado
+# analisar de uma forma que seja possível
+# trilhar um caminho até locais mais próximos 
+# ao local inacessível, uma solução é tornar também
+# a vizinhança marcada como local inacessível (como usarei por hora):
+mapa = [['R','0','0','0','0' , '0'],
+        ['0','1','0','1','-1','-1'],
+        ['0','0','0','0','-1','-1']]
+grf = g.G(mapa)
+grf.toString()
